051d9766b1568e015ada090b8c501ed47be8fbaf
10daisuke64 <daisukesumida64@gmail.com>
2021-12-17 00:12:37 +0900

    first_commit

---
+++ liquild-hover-effect/js/script.js
@@ -0,0 +1,14 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "./img/1.jpg",
+  image2: "./img/2.jpg",
+  // speedIn: 2,
+  // speedOut: 5,
+  angle: Math.PI / 8,
+  // displacementImage: "./img/strip.png"
+  // displacementImage: "./img/heightMap.png"
+  displacementImage: "./img/fiber.png"
+  // displacementImage: "./img/cloud.png"
+  // displacementImage: "./img/stripe1.png"
+});
